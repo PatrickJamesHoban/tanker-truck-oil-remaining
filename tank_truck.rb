e79629a8dd3740a9498353b94c472e69f0706a3b
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-02-06 15:54:02 -0600

    updated order of code in solution file

--- tank_truck.rb
+++ tank_truck.rb
@@ -1,28 +1,21 @@
-=begin
-To introduce the problem think to my neighbor who drives a tanker truck. The level indicator is down and he is worried because he does not know if he will be able to make deliveries. We put the truck on a horizontal ground and measured the height of the liquid in the tank.
-
-Fortunately the tank is a perfect cylinder and the vertical walls on each end are flat. The height of the remaining liquid is h, the diameter of the cylinder is d, the total volume is vt (h, d, vt are positive or null integers). You can assume that h <= d.
-
-Could you calculate the remaining volume of the liquid? Your function tankvol(h, d, vt) returns an integer which is the truncated result (e.g floor) of your float calculation.
-
-Fortunately the tank is a perfect cylinder and the vertical walls on each end are flat. The height of the remaining liquid is h, the diameter of the cylinder is d, the total volume is vt (h, d, vt are positive or null integers). You can assume that h <= d.
-=end
-
 # PSEUDOCODE
-# V=πr2L
-# V=L(R2cos−1(R−hR)−(R−h)2Rh−h2‾‾‾‾‾‾‾‾‾√)  gives the volume from a radius R, a height h and a length L.
 
-# Deconstruct vt to find L in above equation
-# L = (vt / Math.acos(-1)) / (Math.sqrt(radius))
-# pi in ruby is Math.acos(-1) => 3.14...
-# cos−1(1−x)+x(2−x)‾‾‾‾‾‾‾‾√(x−1) where x = h/r
-# Circle area = pi r sqrd
-# Circumference = 2*pi*r
-      # without the l, should give the area of the portion of the circle filled.  Take the portion against the total circle area and use that percent times the total volume?
+  # V=πr2L
+  # V=L(R2cos−1(R−hR)−(R−h)2Rh−h2‾‾‾‾‾‾‾‾‾√)  gives the volume from a radius R, a height h and a length L.
+
+  # Deconstruct vt to find L in above equation
+  # L = (vt / Math.acos(-1)) / (Math.sqrt(radius))
+  # pi in ruby is Math.acos(-1) => 3.14...
+  # cos−1(1−x)+x(2−x)‾‾‾‾‾‾‾‾√(x−1) where x = h/r
+  # Circle area = pi r sqrd
+  # Circumference = 2*pi*r
+        # without the l, should give the area of the portion of the circle filled.  Take the portion against the total circle area and use that percent times the total volume?
 
 
 # CODE
-# REFACTOR
+
+# REFACTORED CODE
+
 def tankvol(h, d, vt)
   radius = d.to_f/2
   l = vt / (Math.acos(-1)*radius**2)
@@ -30,7 +23,20 @@ def tankvol(h, d, vt)
   cylindersectorarea.floor
 end
 
-# FIRST ATTEMPT
+
+# DRIVER CODE
+
+p tankvol(60,120,3500) # should return 1750
+p tankvol(40,120,3500) # should return 1021 (calculation gives about: 1021.26992027)
+p tankvol(80,120,3500) # should return 2478 (calculation gives about: 2478.73007973)
+p tankvol(5, 7, 3848) # should return 2940
+
+
+
+# OLD CODE
+
+# THOUGHT PROCESS / FIRST ATTEMPT
+
 # def tankvol(h, d, vt)
 #   heightremaining = h # portion of the radius
 #   p 'radius is ...'
@@ -109,9 +115,3 @@ end
   
 # end
 
-
-# DRIVER CODE
-# p tankvol(60,120,3500) # should return 1750
-# p tankvol(40,120,3500) # should return 1021 (calculation gives about: 1021.26992027)
-# p tankvol(80,120,3500) # should return 2478 (calculation gives about: 2478.73007973)
-p tankvol(5, 7, 3848) # should return 2940
